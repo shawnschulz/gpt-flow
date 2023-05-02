c5de82f265f71f1d53e276d9bc148b3ce58ab1a8
Shawn Schulz <shawnschulz@DN0a25a805.SUNet>
2023-05-02 11:59:43 -0700

    added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import ReactFlow, {
   useNodesState,
   useEdgesState,
   addEdge,
+  EdgeText
 } from 'reactflow';
 
 import 'reactflow/dist/style.css';
+import { useState } from 'react';
 
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
@@ -19,22 +21,39 @@ const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [textboxValue, setTextboxValue] = useState('');
 
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
-
+  function downloadJsonButton() {
+    alert('Download not available yet');
+  }
+  function runFlowButton() {
+    alert('runFlow not working yet');
+  }
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+      <button onClick={runFlowButton}>Run flow</button>
+      <div style={{float: 'right'}}>
+        <button onClick={downloadJsonButton}>Download Flow as Json</button>
+      </div>
       <ReactFlow
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        
       >
         <Controls />
         <MiniMap />
         <Background variant="dots" gap={12} size={1} />
+        <input type="text" value={textboxValue} onChange={e => setTextboxValue(e.target.value)} />
+        
       </ReactFlow>
+      
     </div>
   );
+
+  // Usage
+
 }
