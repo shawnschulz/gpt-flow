b5041ba15c92902351352e7344f56d03c4260acc
Shawn Schulz <94928969+shawnschulz@users.noreply.github.com>
2023-05-05 12:11:05 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,20 @@ import ReactFlow, {
 import 'reactflow/dist/style.css';
 import { useState } from 'react';
 
+const exportToJson = (nodes, edges) => {
+  const data = {
+    nodes: nodes,
+    edges: edges,
+  };
+  const fileData = JSON.stringify(data);
+  const blob = new Blob([fileData], { type: "text/plain;charset=utf-8" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "nodes_edges_data.json";
+  link.click();
+};
+
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
@@ -30,16 +44,22 @@ export default function App() {
   const [textboxValue, setTextboxValue] = useState('');
 
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+<<<<<<< HEAD
+  function downloadJsonButton(nodes, edges) { 
+=======
   function downloadJsonButton(nodes, edges, nodeInfo = "this might not be necessary if we can just store attributes in node class") { 
+>>>>>>> refs/remotes/origin/main
     
-    var downloadJsonString = 'Download not working yet'
-    alert(downloadJsonString);
-    var jsonDict = {'test': downloadJsonString}
-    var dictstring = JSON.stringify(jsonDict)
+    exportToJson(nodes, edges)
   }
 
+<<<<<<< HEAD
+  function runFlowButton() {
+    var runFlowString = JSON.stringify(edges)
+=======
   function runFlowButton(nodes, edges) {
     var runFlowString = 'runFlow not working yet'
+>>>>>>> refs/remotes/origin/main
     alert(runFlowString);
     initialEdges[0]['']
 
@@ -47,6 +67,13 @@ export default function App() {
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+<<<<<<< HEAD
+      <button onClick={() => runFlowButton(nodes, edges)}>Run flow</button>
+      <div style={{float: 'right'}}>
+        <button onClick={() => downloadJsonButton(nodes, edges)}>Download Flow as Json</button>
+      </div>
+=======
+>>>>>>> refs/remotes/origin/main
       <input type="text" value={textboxValue} onChange={e => setTextboxValue(e.target.value)} />
       <ReactFlow
         nodes={nodes}
@@ -62,10 +89,11 @@ export default function App() {
       <button onClick={() => runFlowButton(nodes, edges)}>Run flow</button>
         <Controls />
         <Background variant="dots" gap={12} size={1} />
-        
+        <button onClick={exportToJson}>Export JSON</button>
+
         
       </ReactFlow>
-      
+
     </div>
   );
 
