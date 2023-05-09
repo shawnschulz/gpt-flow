cff803201c657887d04629c08e1b7f6553cabcf2
shawnschulz <shawnschulz@berkeley.edu>
2023-05-09 15:21:04 -0700

    adding proper json exporting

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,11 @@ import ReactFlow, {
 } from 'reactflow';
 
 import { MarkerType, Position } from 'reactflow';
-
 import 'reactflow/dist/style.css';
 import { useState } from 'react';
 //import CustomNode from './CustomNode'
 
-const exportToJson = (data) => {
+const exportToJson = (dictionary) => {
   //This just turns dictionary style objects itno a json and then downloads it
   const fileData = JSON.stringify(data);
   const blob = new Blob([fileData], { type: "text/plain;charset=utf-8" });
@@ -40,9 +39,11 @@ export default function App() {
   const [textboxValue, setTextboxValue] = useState('');
 
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
-  function downloadJsonButton(data) { 
+  
+  //Buttons
+  function downloadJsonButton(dictionary) { 
     
-    exportToJson(data)
+    exportToJson(dictionary)
   }
 
   function runFlowButton(data) {
@@ -52,8 +53,35 @@ export default function App() {
 
   }
 
-  function makeJsonData(nodes, edges){
+  function makeJsonData(data){
     // Makes a properly formatted json given the node info and edges THIS MAY NEED TO CHANGE TO TAKE CUSTOM NODE INFO INPUT
+    // I think the Json itself has to be nested, either a list of dictionaries or dictionary of dictionaries, lets try list first
+    //const jsonDict = {
+    //  nodeID: 
+     // nodeInfo: {
+      //position:"",
+      //type: "",
+      //values: [{value:"", valueId:"", nodeId:""}]
+      //}
+    //}
+    // loop through nodes in nodes
+    let temp_nodes = data[nodes]
+    let temp_edges = data[edges]
+    const jsonDict = new Object();
+    loopIterationCounter = 0
+    for (let node in temp_nodes[nodes]) {
+      jsonDict[nodeID] = node[id]
+      jsonDict[nodeID][nodeInfo] = new Object()
+      jsonDict[nodeID][nodeInfo][position] = node[position]
+      jsonDict[nodeID][nodeInfo][type] = node[type]
+      jsonDict[nodeID][nodeInfo][values] = node[values]
+      // this should hopefully get all the next temp_nodes in the correct order, could be a source of bugs tho 
+      if (loopIterationCounter < temp_edges.length){
+        jsonDict[nodeID][nodeInfo][edges] = temp_edges[loopIterationCounter][target]
+      }
+    }
+    alert(JSON.stringify(data));
+    return jsonDict;
   }
 
   const data = {
@@ -64,7 +92,7 @@ export default function App() {
     <div style={{ width: '100vw', height: '100vh' }}>
       <button onClick={() => runFlowButton(data)}>Run flow</button>
       <div style={{float: 'right'}}>
-        <button onClick={() => downloadJsonButton(data)}>Download Flow as Json</button>
+        <button onClick={() => downloadJsonButton(makeJsonData(data))}>Download Flow as Json</button>
       </div>
       <input type="text" value={textboxValue} onChange={e => setTextboxValue(e.target.value)} />
       <ReactFlow
