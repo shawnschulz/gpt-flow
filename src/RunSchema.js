460ba4d1b1eff308062ce84608ed694ea37dec16
Shawn <shawnschulz@berkeley.edu>
2023-11-04 15:55:11 -0700

    got message bot to work

--- src/RunSchema.js
+++ src/RunSchema.js
@@ -27,27 +27,27 @@ export default runSchema
       return(return_dictionary)
  }
 
-export async function promptOneMessage(prompt){
-    async function sendMessageAndProcess(prompt){
-        try{
-            let prompt_text = prompt;
-            const response = await fetch('http://127.0.0.1:4269/message_json_handler', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'text',
-                },
-                body: prompt_text 
-            }
-            );
-            if(!response.ok){
-                throw new Error(`HTTP error! status ${response.status}`);
-            }
-            const result = await response.json();
-            return result
+ export async function promptOneMessage(listedSchemaDict) {
+      async function sendDictionaryAndProcess(listedSchemaDict) {
+        try {
+	  let json_string = JSON.stringify(listedSchemaDict)
+          const response = await fetch('http://127.0.0.1:4269/message_json_handler', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: json_string
+          }
+          );
+          if(!response.ok) {
+            throw new Error(`HTTP error! status ${response.status}`);
+          }
+          const result = await response.json();
+          return result;
         } catch (error) {
-            console.error('There was a probelm prompting the LLM with your message:', error)
+          console.error('There was a problem running the schema through graph traversal:', error)
         }
-    }
-    let return_message = await sendMessageAndProcess(prompt);
-    return(return_message)
-}
+      }
+      let return_dictionary = await sendDictionaryAndProcess(listedSchemaDict);
+      return(return_dictionary)
+ }
--- src/bot/MessageParser.js
+++ src/bot/MessageParser.js
@@ -1,20 +1,43 @@
-import { promptOneMessage } from "../RunSchema";
-
 class MessageParser {
   constructor(actionProvider, state) {
     this.actionProvider = actionProvider;
     this.state = state;
+    this.promptOneMessage = promptOneMessage
   }
 
     // When message entered, need to get value by calling promptOneMessage from RunSchema, awaiting for the result,
         // then use actionprovider to render the message
   parse(message) {
-
-    const llmOutput = promptOneMessage(message)
-    this.actionProvider.outputText(llmOutput)
+    let promptDict = {'prompt':message}
+    promptOneMessage(promptDict).then(res => this.actionProvider.outputText(res))
 
   }
 }
 
+ export async function promptOneMessage(listedSchemaDict) {
+      async function sendDictionaryAndProcess(listedSchemaDict) {
+        try {
+	  let json_string = JSON.stringify(listedSchemaDict)
+          const response = await fetch('http://127.0.0.1:4269/message_json_handler', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: json_string
+          }
+          );
+          if(!response.ok) {
+            throw new Error(`HTTP error! status ${response.status}`);
+          }
+          const result = await response.json();
+          return result;
+        } catch (error) {
+          console.error('There was a problem running the schema through graph traversal:', error)
+        }
+      }
+      let return_dictionary = await sendDictionaryAndProcess(listedSchemaDict);
+     console.log("Prompting successful, here was the result:" + return_dictionary["Response"])
+      return(return_dictionary["Response"])
+ }
 
 export default MessageParser;
