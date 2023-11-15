0a7d0ece4d975bef9ae44517ffee665a401f7ab3
Shawn <shawnschulz@berkeley.edu>
2023-11-14 17:45:09 -0800

    added error page and router, should be able to make new pages with this

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom"
+import ErrorPage from './ErrorPage.tsx'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />, 
+        errorElement: <ErrorPage />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
